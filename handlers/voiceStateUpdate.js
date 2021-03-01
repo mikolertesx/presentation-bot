898d21e91718c71cf7c64349341fb623da52b576
Mike <mgangsal@outlook.com>
2021-02-28 20:00:36 -0600

    Hi it's me on left alone, and on alone user.

--- handlers/voiceStateUpdate.js
+++ handlers/voiceStateUpdate.js
@@ -1,12 +1,24 @@
-module.exports = async (_, newState) => {
-  if (
-    newState.member.voice.channel &&
-    newState.member.voice.channel.members.size === 1
-  ) {
-    console.log("Joined a call");
-    const connection = await newState.member.voice.channel.join();
-    connection.play("./audio/hi.mp3").on("finish", () => {
-      connection.disconnect();
-    });
+module.exports = async (oldState, newState) => {
+  const playAudio = async (voiceChannel) => {
+    try {
+      const connection = await voiceChannel.join();
+      connection.play("./audio/hi.mp3").on("finish", () => {
+        connection.disconnect();
+      });
+    } catch (err) {
+      console.error("Couldn't join");
+    }
+  };
+
+  if (newState.member.user.bot || oldState.member.user.bot) {
+    return;
+  }
+
+  if (newState.channel && newState.channel.members.size === 1) {
+    return playAudio(newState.channel);
+  }
+
+  if (oldState.channel && oldState.channel.members.size === 1) {
+    return playAudio(oldState.channel);
   }
 };
